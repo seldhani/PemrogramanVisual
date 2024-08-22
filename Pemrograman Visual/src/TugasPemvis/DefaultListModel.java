48608f4b22ff07b82ec5d1ff83fb159991a4cf5f
seldhani <113575184+seldhani@users.noreply.github.com>
2024-08-22 13:55:39 +0700

    Add files via upload
    Modul 5-10
    
    //22 Agustus 2024
    Modul 7 belum sempurna, modul 8 belum dikerjakan

---
+++ Pemrograman Visual/src/TugasPemvis/DefaultListModel.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package TugasPemvis;
+
+/**
+ *
+ * @author Dr. Kudrat Sunandar
+ */
+class DefaultListModel<T> {
+
+    void addElement(String sprite) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int getSize() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void remove(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void insertElementAt(String text, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void add(int i, String sprite) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
